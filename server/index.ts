4c7d75c8357a25fd2978955dbd0b666c8e0e6951
Icksan <icksanmambote@gmail.com>
2025-08-10 00:39:38 +0100

    post
    flemme

--- server/index.ts
+++ server/index.ts
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // Listen on all interfaces
+  // Listen on localhost (Windows local)
   const port = parseInt(process.env.PORT || "5000", 10);
   app.listen(port, "0.0.0.0", () => {
     log(`Server running on http://0.0.0.0:${port}`);
