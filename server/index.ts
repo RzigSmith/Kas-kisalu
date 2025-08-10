39d5aacf08e0c8f9ed49698dc2d990204b833631
Icksan <icksanmambote@gmail.com>
2025-08-10 22:21:03 +0100

    server

--- server/index.ts
+++ server/index.ts
@@ -59,8 +59,8 @@ app.use((req, res, next) => {
 
   // Listen on localhost (Windows local)
   const port = parseInt(process.env.PORT || "5000", 10);
-  app.listen(port, "127.0.0.1", () => {
-    log(`Server running on http://127.0.0.1:${port}`);
+  app.listen(port, "0.0.0.0", () => {
+    log(`Server running on http://0.0.0.0:${port}`);
   });
 })();
 
