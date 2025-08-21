c63da95527e7c92e9fbca0821ff0043ff6285966
Icksan <icksanmambote@gmail.com>
2025-08-21 19:52:19 +0100

    push

--- server/index.js
+++ server/index.js
@@ -135,5 +135,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: err.message || "Erreur serveur" });
 });
 
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
